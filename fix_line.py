42dc6c644eef0b1614156aee4f695397ec294535
B Mercurius <merkur42@googlemail.com>
2026-01-28 16:10:21 +0100

    Remove obsolete temporary script fix_line.py

--- fix_line.py
+++
@@ -1,6 +0,0 @@
-import os
-with open('src/main/resources/templates/base.html', 'r') as f: lines = f.readlines()
-with open('src/main/resources/templates/base.html', 'w') as f:
-    for line in lines:
-        if 'details' in line:
-            f.write('    <details { 0f child.isParentOf(currentTagName) 
